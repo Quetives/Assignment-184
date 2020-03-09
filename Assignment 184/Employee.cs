7665b6ccd57b1778693b3a2e6c3897335cbef149
Richard <40587587+Quetives@users.noreply.github.com>
2020-03-09 14:19:56 -0700

    Add project files.

---
+++ Assignment 184/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperator
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+
+        public Employee(int id, string fn, string ln)
+        {
+            Id = id;
+            FirstName = fn;
+            LastName = ln;
+        }
+
+        //The comparison operators must be overloaded in pairs.
+        //That is, if either operator of a pair is overloaded, 
+        //the other operator must be overloaded as well.
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            return employee1.Id == employee2.Id;
+        }
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return employee1.Id != employee2.Id;
+        }
+    }
+}
---
+++ Assignment 184/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee(1, "Richard", "Quetives");
+            Employee emp2 = new Employee(1, "Dick", "Q");
+            Employee emp3 = new Employee(2, "Anahiza", "Carrillo");
+
+            Console.WriteLine("Are {0} {1} and {2} {3} the same person? \n" + (emp1 == emp2), emp1.FirstName, emp1.LastName, emp2.FirstName, emp2.LastName);
+            Console.ReadLine();
+
+            Console.WriteLine("Are {0} {1} and {2} {3} the same person? \n" + (emp1 == emp3), emp1.FirstName, emp1.LastName, emp3.FirstName, emp3.LastName);
+            Console.ReadLine();
+
+            Console.WriteLine("Are {0} {1} and {2} {3} different people? \n" + (emp2 != emp3), emp2.FirstName, emp2.LastName, emp3.FirstName, emp3.LastName);
+            Console.ReadLine();
+        }
+    }
+}
